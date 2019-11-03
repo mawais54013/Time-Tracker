8a71ad318746e76d4b51cb6fd705ef0c979d626b
MAwais <mawais54013@outlook.com>
2019-11-02 19:55:47 -0700

    Initial commit

---
+++ Time Tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Time Tracker
+//
+//  Created by muhammad Awais on 11/2/19.
+//  Copyright © 2019 muhammad Awais. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
